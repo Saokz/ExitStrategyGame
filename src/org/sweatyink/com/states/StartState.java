12fa6451d28a6d8128e1f5896546c402e0c66520
Kenny Stepney <kennystepney@hotmail.com>
2015-11-07 15:07:00 -0600

    Create StartState.java

---
+++ src/org/sweatyink/com/states/StartState.java
@@ -0,0 +1,19 @@
+package org.sweatyink.com.states;
+
+import java.awt.Graphics;
+
+public class StartState extends GameState{
+
+	public StartState(){
+		
+	}
+	
+	public void tick() {
+		
+	}
+
+	public void render(Graphics g) {
+		
+	}
+	
+}
