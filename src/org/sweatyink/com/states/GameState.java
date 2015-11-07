ad9b7c74d94ba4f11cf94a2dea8ea129de1486b5
Kenny Stepney <kennystepney@hotmail.com>
2015-11-07 15:48:07 -0600

    Update GameState.java

--- src/org/sweatyink/com/states/GameState.java
+++ src/org/sweatyink/com/states/GameState.java
@@ -3,14 +3,17 @@ package org.sweatyink.com.states;
 import java.awt.Graphics;
 
 import org.sweatyink.com.InputHandler;
+import org.sweatyink.com.core.GameStateManager;
 
 public abstract class GameState {
 	protected int ID;
 	protected String name;
 	protected InputHandler i;
+	protected GameStateManager m;
 	
-	public GameState(InputHandler i){
+	public GameState(InputHandler i, GameStateManager m){
 		this.i = i;
+		this.m = m;
 	}
 	
 	public abstract void tick();
