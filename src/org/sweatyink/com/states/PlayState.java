2228c31ee556d5156bd24cbf09f0ee5733e2d768
Kenny Stepney <kennystepney@hotmail.com>
2015-11-07 16:20:12 -0600

    Update PlayState.java

--- src/org/sweatyink/com/states/PlayState.java
+++ src/org/sweatyink/com/states/PlayState.java
@@ -4,19 +4,28 @@ import java.awt.Graphics;
 
 import org.sweatyink.com.InputHandler;
 import org.sweatyink.com.core.GameStateManager;
+import org.sweatyink.com.core.HUD;
+import org.sweatyink.com.core.Handler;
 
 public class PlayState extends GameState{
+	private Handler h;
+	private HUD hud;
 
-	public PlayState(InputHandler i, GameStateManager m) {
-		super(i, m);
+	public PlayState(String name, int ID, InputHandler i, GameStateManager m) {
+		super(name, ID, i, m);
+		
+		h = new Handler();
+		hud = new HUD();
 	}
 
 	public void tick() {
-		
+		h.tick();
+		hud.tick();
 	}
 
 	public void render(Graphics g) {
-		
+		h.render(g);
+		hud.render(g);
 	}
 
 }
