9de7f07cdb0cad8f33a3cc8d66bc11b87c82219e
Kenny Stepney <kennystepney@hotmail.com>
2015-11-07 15:50:57 -0600

    Create PlayState.java

---
+++ src/org/sweatyink/com/states/PlayState.java
@@ -0,0 +1,22 @@
+package org.sweatyink.com.states;
+
+import java.awt.Graphics;
+
+import org.sweatyink.com.InputHandler;
+import org.sweatyink.com.core.GameStateManager;
+
+public class PlayState extends GameState{
+
+	public PlayState(InputHandler i, GameStateManager m) {
+		super(i, m);
+	}
+
+	public void tick() {
+		
+	}
+
+	public void render(Graphics g) {
+		
+	}
+
+}
