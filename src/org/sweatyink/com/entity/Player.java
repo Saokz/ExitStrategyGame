6f1c182de45907d4eb28fe30361870bccfa2d2fe
RockersDriven <rygarcarvajal@yahoo.com>
2015-11-07 20:46:04 -0600

    Update Player.java

--- src/org/sweatyink/com/entity/Player.java
+++ src/org/sweatyink/com/entity/Player.java
@@ -1,17 +1,30 @@
 package org.sweatyink.com.entity;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
+
+import org.sweatyink.com.GameMain;
+import org.sweatyink.com.core.Handler;
+import org.sweatyink.com.states.PlayState;
 
 public class Player extends GameObject implements KeyListener{
-	public Player(int playerX, int playerY, int playerHeight, int playerWidth)
+	private GameMain gm;
+	private Handler h;
+	private PlayState ps;
+	
+	public Player(int playerX, int playerY, int playerHeight, int playerWidth, GameMain g, Handler h, PlayState ps)
 	{
 		super(playerX, playerY, playerHeight, playerWidth);
 		x = playerX;
 		y = playerY;
 		height = playerHeight;
 		width = playerWidth;
+		gm = g;
+		this.h = h;
+		this.ps = ps;
+		g.addKeyListener(this);
 	}
 	
 	public int getX(){return x;}
@@ -30,16 +43,32 @@ public class Player extends GameObject implements KeyListener{
 			moveRight();
 		}
 		
-		if (e.getKeyCode() == KeyEvent.VK_UP)
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
 		{
 			moveUp();
 		}
 		
-		if(e.getKeyCode() == KeyEvent.VK_DOWN)
+		if(e.getKeyCode() == KeyEvent.VK_UP)
 		{
 			moveDown();
 		}
 		
+	}
+	
+	public void tick(){
+		for(int i = 0; i < h.objects.size(); i++){
+			GameObject o = h.objects.get(i);
+			if(o instanceof Walls){
+				if(o.getBounds().intersects(getBounds())){
+					ps.reset(gm);
+				}
+			}
+		}
+	}
+	
+	public void render(Graphics g){
+		g.setColor(Color.RED);
+		g.fillRect(x, y, width, height);
 	}
 
 	@Override
@@ -54,9 +83,9 @@ public class Player extends GameObject implements KeyListener{
 		
 	};
 	
-	private void moveLeft(){x--;}
-	private void moveRight(){x++;}
-	private void moveUp(){y++;}
-	private void moveDown(){y--;}
+	private void moveLeft(){x -= 20;}
+	private void moveRight(){x += 20;}
+	private void moveUp(){y += 20;}
+	private void moveDown(){y -= 20;}
 	
 }
